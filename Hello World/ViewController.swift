0c915bf11f86d1a7c79a14494697e287bb8a509d
SHUM <cmeloctb@icloud.com>
2020-12-01 23:33:28 +0300

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Aleksey Ivantsov on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
