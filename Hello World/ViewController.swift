92be05d40b18d142d0f200fd26d5ae0fa360156d
SHUM <cmeloctb@icloud.com>
2020-12-02 01:05:49 +0300

    logic button

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelText.isHidden = true
+        toggleButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func showLabel(_ sender: UIButton) {
+        if labelText.isHidden {
+            labelText.isHidden = false
+            toggleButton.setTitle("text OFF", for: .normal)
+        } else {
+            labelText.isHidden = true
+            toggleButton.setTitle("text ON", for: .normal)
+        }
+    }
 }
 
